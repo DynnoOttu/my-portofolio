81eac145ffd0ade1fcdf55500add19a1f1d255d4
DynnoOttu <dynnoottu968@gmail.com>
2023-07-13 20:26:55 +0800

    about section

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+        dark: '#1e293b',
+        secondary: '#64749b'
+      },
+      screens: {
+        '2xl': '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
+
